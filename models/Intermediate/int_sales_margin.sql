df15b7c08ad084feee3b5d8c49f618af789d77aa
ouhaiba dilmi <ouhaiba.dilmi@gmail.com>
2025-05-07 15:57:57 +0000

    margin

--- models/Intermediate/int_sales_margin.sql
+++ models/Intermediate/int_sales_margin.sql
@@ -1,4 +1,6 @@
-SELECT (quantity*purchase_price)as purchase_cost
+SELECT (quantity*purchase_price)as purchase_cost,
+(revenue-(quantity*purchase_price)) as margin,
 FROM {{ ref('stg_gz_raw_data__raw_gz_sales')}} as sales
 LEFT JOIN {{ ref("stg_gz_raw_data__raw_gz_product")}} as product
 ON sales.products_id=product.products_id 
+ 
